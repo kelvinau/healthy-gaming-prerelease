cb95f449871e0ee62d647846a04119ec3462c36f
Kelvin Au <kelvinau4413@hotmail.com>
2018-05-13 01:17:17 -0700

    minor change to image titles

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ $COUNTRY_LIST = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla"
                         <a class="nav-link" href="#faq">FAQ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link background-img controller" href="#signup" title="Sign Up"></a>
+                        <a class="nav-link background-img controller" href="#signup" title="Click here to sign up"></a>
                     </li>
                 </ul>
             </div>
@@ -105,7 +105,7 @@ $COUNTRY_LIST = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla"
             <div class="section founder background-img background-2" data-anchor="founder" id="section-founder">
                 <div class="content-wrapper row">
                     <div class="col-4 founder-img-container">
-                        <img class="founder-img" src="image/founders.jpg">
+                        <img class="founder-img" src="image/founders.jpg" title="HealthyGaming-founders">
                     </div>
                     <div class="col-8">
                         <div class="founder-description">
@@ -171,7 +171,7 @@ $COUNTRY_LIST = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla"
                         </div>
                         <input name="csrf_token" id="csrf_token" value="<?= $_SESSION['csrf_token'] ?>" type="hidden">
                         <div class="submit-container">
-                            <button type="submit" class="background-img signup_btn" title="Click Here to Register" id="signUp-btn"></button>
+                            <button type="submit" class="background-img signup_btn" title="Click here to register" id="signUp-btn"></button>
                             <p class="error-msg"></p>
                         </div>
                     </form>
